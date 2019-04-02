39df37b864329b707913b09a5aa8b44bccbf5a92
Eric Rosebrock <webfreakeric@gmail.com>
2019-04-02 17:07:56 -0400

    Fix paginator from not updating

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,5 +1,4 @@
-const Post = require('../models/post');
-
+const Post = require("../models/post");
 
 exports.getPosts = (req, res, next) => {
   const pageSize = +req.query.pageSize;
@@ -7,35 +6,38 @@ exports.getPosts = (req, res, next) => {
   const postQuery = Post.find();
   let fetchedPosts = [];
 
-  if(pageSize && currentPage) {
+  if (pageSize && currentPage) {
     postQuery
       .skip(pageSize * (currentPage - 1))
       .limit(pageSize)
-      .sort('-createdAt');
+      .sort("-createdAt");
   }
 
-  postQuery.then(documents => {
-    fetchedPosts = documents;
-    return Post.count();
-  }).then(count => {
-    res.status(200).json({
-      message: 'Sent Posts Success',
-      posts: fetchedPosts,
-      maxPosts: count
+  postQuery
+    .then(documents => {
+      fetchedPosts = documents;
+      return Post.estimatedDocumentCount();
+    })
+    .then(count => {
+      res.status(200).json({
+        message: "Sent Posts Success",
+        posts: fetchedPosts,
+        maxPosts: count
+      });
+    })
+    .catch(error => {
+      res.status(500).json({ message: error.message });
     });
-  }).catch(error => {
-    res.status(500).json({ message: error.message });
-  });
 };
 
 exports.getPost = (req, res, next) => {
   Post.findById(req.params.id)
     .then(post => {
-      if(post) {
+      if (post) {
         res.status(200).json(post);
       } else {
         // console.log('Post missing');
-        res.status(404).json({ message: 'Post not found' });
+        res.status(404).json({ message: "Post not found" });
       }
     })
     .catch(error => {
@@ -44,35 +46,37 @@ exports.getPost = (req, res, next) => {
 };
 
 exports.createPost = (req, res, next) => {
-  const post =  new Post({
+  const post = new Post({
     title: req.body.title,
     content: req.body.content,
-    imagePath: '/images/posts/' + req.file.filename,
+    imagePath: "/images/posts/" + req.file.filename,
     user: req.userData.userId
   });
 
-  post.save().then(result => {
-    res.status(201).json({
-      message: 'Success',
-      postId: result._id,
-      post: {
-        id: result._id,
-        title: result.title,
-        content: result.content,
-        createdAt: result.createdAt,
-        imagePath: result.imagePath
-      }
+  post
+    .save()
+    .then(result => {
+      res.status(201).json({
+        message: "Success",
+        postId: result._id,
+        post: {
+          id: result._id,
+          title: result.title,
+          content: result.content,
+          createdAt: result.createdAt,
+          imagePath: result.imagePath
+        }
+      });
+    })
+    .catch(error => {
+      res.status(500).json({ message: error.message });
     });
-  })
-  .catch(error => {
-    res.status(500).json({ message: error.message });
-  });
 };
 
 exports.updatePost = (req, res, next) => {
   let imagePath = req.body.imagePath;
-  if(req.file) {
-    imagePath = '/images/posts/' + req.file.filename;
+  if (req.file) {
+    imagePath = "/images/posts/" + req.file.filename;
   }
 
   const post = new Post({
@@ -85,29 +89,29 @@ exports.updatePost = (req, res, next) => {
 
   delete post.createdAt; // do not update the date
 
-  Post.updateOne({_id: req.params.id, user: req.userData.userId}, post).then(result => {
-    if(result.n > 0) {
-      res.status(200).json({ message: 'Update Successful' });
-    } else {
-      res.status(401).json({ message: 'Not Authorized' });
-    }
-  })
-  .catch((err) => {
-    res.status(500).json({ message: err.message });
-  });
+  Post.updateOne({ _id: req.params.id, user: req.userData.userId }, post)
+    .then(result => {
+      if (result.n > 0) {
+        res.status(200).json({ message: "Update Successful" });
+      } else {
+        res.status(401).json({ message: "Not Authorized" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: err.message });
+    });
 };
 
 exports.deletePost = (req, res, next) => {
-  Post.deleteOne({_id: req.params.id, user: req.userData.userId}).then(result => {
-    if(result.n > 0) {
-      res.status(200).json({ message: 'Post Deleted' });
-    } else {
-      res.status(401).json({ message: 'Not Authorized' });
-    }
-  })
-  .catch(err => {
-    res.status(200).json({ message: 'Failed to delete Post' });
-  });
+  Post.deleteOne({ _id: req.params.id, user: req.userData.userId })
+    .then(result => {
+      if (result.n > 0) {
+        res.status(200).json({ message: "Post Deleted" });
+      } else {
+        res.status(401).json({ message: "Not Authorized" });
+      }
+    })
+    .catch(err => {
+      res.status(200).json({ message: "Failed to delete Post" });
+    });
 };
-
-
